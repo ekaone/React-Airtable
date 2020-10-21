d6c3401975f71cdabd0193f00842688e4de5e198
Eka Prasetia <ekaone3033@gmail.com>
2020-10-21 11:29:19 +0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Airtable
+
+## Issue Using in browser
+
+`TypeError: Failed to execute 'fetch' on 'Window': Illegal invocation`
+
+Solution open /node_modules/airtable/lib/fetch.js, on line 8, change
+```
+typeof window === 'undefined' ? node_fetch_1.default : fetch;
+```
+to
+```
+typeof window === 'undefined' ? node_fetch_1.default : window.fetch.bind(window));
+```
--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Airtable from "airtable";
 
 function App() {
-  return <>Air table</>;
+  const base = new Airtable({ apiKey: "keypSI9YKcvGIQVLa" }).base(
+    "appJxKLdSK7M3cwii"
+  );
+
+  useEffect(() => {
+    base("goals")
+      .select({ view: "Grid view" })
+      .eachPage((records, fetchNextPage) => {
+        console.log(records);
+        fetchNextPage();
+      });
+  }, []);
+
+  return <>Air table base key</>;
 }
 
 export default App;
